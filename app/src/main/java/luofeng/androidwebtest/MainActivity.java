e2f06209cf4b78c99354b4f74fe4669af9e4e17c
luofeng <luofengliuchen@gmail.com>
2016-02-06 14:21:37 +0800

    Initial commit

---
+++ app/src/main/java/luofeng/androidwebtest/MainActivity.java
@@ -0,0 +1,30 @@
+package luofeng.androidwebtest;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webview = (WebView) findViewById(R.id.wv);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setBackgroundColor(Color.TRANSPARENT);
+        webview.loadUrl("file:///android_asset/canvas2.html");
+//        webview.loadUrl("http://www.liuschen.com/");
+
+    }
+
+}
