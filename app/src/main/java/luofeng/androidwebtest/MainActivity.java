76b75638f35766f127825bc1f6ad792bc3cfa82e
luofeng <luofengliuchen@gmail.com>
2016-02-15 18:17:00 +0800

    loading test

--- app/src/main/java/luofeng/androidwebtest/MainActivity.java
+++ app/src/main/java/luofeng/androidwebtest/MainActivity.java
@@ -2,16 +2,8 @@ package luofeng.androidwebtest;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
